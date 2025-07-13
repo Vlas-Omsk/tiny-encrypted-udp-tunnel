2a9b94bbced0f3b9bf2a54fb900f98722a8fd539
Vlas <vlasdergaev@gmail.com>
2025-07-14 00:56:41 +0500

    Add ipv6 support
    Clean

--- forward.cpp
+++ forward.cpp
@@ -6,13 +6,11 @@
 #include<getopt.h>
 #include <unistd.h>
 #include<errno.h>
-
 #include <fcntl.h>
 //#include"aes.h"
-
 #include <sys/epoll.h>
 #include <sys/wait.h>
-
+#include<signal.h>
 #include<map>
 #include<string>
 #include<vector>
@@ -34,13 +32,10 @@ void handler(int num) {
 			//printf("The child exit with code %d",WEXITSTATUS(status));
 		}
 	}
-
 }
+
 void encrypt(char * input, int len, char *key) {
 	int i, j;
-	//char tmp[buf_len];
-	//len=len/16*16+1;
-	//AES128_CBC_encrypt_buffer((uint8_t *)tmp, (uint8_t *)input, len, (uint8_t *)key, (uint8_t *)iv);
 	for (i = 0, j = 0; i < len; i++, j++) {
 		if (key[j] == 0)
 			j = 0;
@@ -49,17 +44,13 @@ void encrypt(char * input, int len, char *key) {
 }
 void decrypt(char * input, int len, char *key) {
 	int i, j;
-	//char tmp[buf_len];
-	//len=len/16*16+1;
-	//AES128_CBC_decrypt_buffer((uint8_t *)tmp, (uint8_t *)input, len, (uint8_t *)key, (uint8_t *)iv);
-	//for(i=0;i<len;i++)
-	//input[i]=tmp[i];
 	for (i = 0, j = 0; i < len; i++, j++) {
 		if (key[j] == 0)
 			j = 0;
 		input[i] ^= key[j];
 	}
 }
+
 void setnonblocking(int sock) {
 	int opts;
 	opts = fcntl(sock, F_GETFL);
@@ -74,8 +65,30 @@ void setnonblocking(int sock) {
 		perror("fcntl(sock,SETFL,opts)");
 		exit(1);
 	}
-
 }
+
+// Helper function to resolve address (IPv4 or IPv6)
+int resolve_addr(const char* addr, int port, struct sockaddr_storage* ss, socklen_t* slen, int* family) {
+	struct addrinfo hints, *res = NULL;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC; // allow IPv4 or IPv6
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_NUMERICHOST;
+
+	char portstr[16];
+	snprintf(portstr, sizeof(portstr), "%d", port);
+	int r = getaddrinfo(addr, portstr, &hints, &res);
+	if (r != 0) {
+		fprintf(stderr, "getaddrinfo failed for %s:%d: %s\n", addr, port, gai_strerror(r));
+		return -1;
+	}
+	memcpy(ss, res->ai_addr, res->ai_addrlen);
+	*slen = res->ai_addrlen;
+	if (family) *family = res->ai_family;
+	freeaddrinfo(res);
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	int i, j, k;
 	int opt;
@@ -96,32 +109,26 @@ int main(int argc, char *argv[]) {
 	}
 	int no_l = 1, no_r = 1;
 	while ((opt = getopt(argc, argv, "l:r:a:b:h")) != -1) {
-		//string opt_key;
-		//opt_key+=opt;
 		switch (opt) {
 		case 'l':
 			no_l = 0;
 			if (strchr(optarg, ':') != 0) {
 				sscanf(optarg, "%[^:]:%d", local_address, &local_port);
 			} else {
-				strcpy(local_address, "127.0.0.1");
+				strcpy(local_address, "::1");
 				sscanf(optarg, "%d", &local_port);
 			}
 			break;
 		case 'r':
 			no_r = 0;
 			if (strchr(optarg, ':') != 0) {
-				//printf("in :\n");
-				//printf("%s\n",optarg);
 				sscanf(optarg, "%[^:]:%d", remote_address, &remote_port);
-				//printf("%d\n",remote_port);
 			} else {
-				strcpy(remote_address, "127.0.0.1");
+				strcpy(remote_address, "::1");
 				sscanf(optarg, "%d", &remote_port);
 			}
 			break;
 		case 'a':
-
 			strcpy(keya, optarg);
 			break;
 		case 'b':
@@ -142,69 +149,103 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	struct sockaddr_in local_me, local_other;
-	int local_listen_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	struct sockaddr_storage local_me, local_other;
+	socklen_t slen_me, slen_other;
+	int addr_family = AF_UNSPEC;
+
+	// resolve local address
+	if (resolve_addr(local_address, local_port, &local_me, &slen_me, &addr_family) != 0) {
+		fprintf(stderr, "Failed to resolve local address\n");
+		exit(1);
+	}
+
+	int local_listen_fd = socket(addr_family, SOCK_DGRAM, 0);
+	if (local_listen_fd < 0) {
+		perror("socket");
+		exit(1);
+	}
 	int yes = 1;
 	setsockopt(local_listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
 
 	char buf[buf_len];
-	socklen_t slen = sizeof(sockaddr_in);
-	memset(&local_me, 0, sizeof(local_me));
-	local_me.sin_family = AF_INET;
-	local_me.sin_port = htons(local_port);
-	local_me.sin_addr.s_addr = inet_addr(local_address);
-	if (bind(local_listen_fd, (struct sockaddr*) &local_me, slen) == -1) {
+	socklen_t slen = slen_me;
+	if (bind(local_listen_fd, (struct sockaddr*) &local_me, slen_me) == -1) {
 		printf("socket bind error");
 		exit(1);
 	}
 	while (1) {
 		socklen_t recv_len;
+		memset(&local_other, 0, sizeof(local_other));
+		slen_other = sizeof(local_other);
 		if ((recv_len = recvfrom(local_listen_fd, buf, buf_len, 0,
-				(struct sockaddr *) &local_other, &slen)) == -1) {
+				(struct sockaddr *) &local_other, &slen_other)) == -1) {
 			printf("recv_from error");
 			exit(1);
 		}
-		printf("Received packet from %s:%d\n", inet_ntoa(local_other.sin_addr),
-				ntohs(local_other.sin_port));
+
+		char addrstr[INET6_ADDRSTRLEN];
+		void *sin_addr = NULL;
+		int port = 0;
+		if (((struct sockaddr *)&local_other)->sa_family == AF_INET) {
+			sin_addr = &((struct sockaddr_in *)&local_other)->sin_addr;
+			port = ntohs(((struct sockaddr_in *)&local_other)->sin_port);
+		} else {
+			sin_addr = &((struct sockaddr_in6 *)&local_other)->sin6_addr;
+			port = ntohs(((struct sockaddr_in6 *)&local_other)->sin6_port);
+		}
+		inet_ntop(((struct sockaddr *)&local_other)->sa_family, sin_addr, addrstr, sizeof(addrstr));
+		printf("Received packet from %s:%d\n", addrstr, port);
 
 		if (keya[0]) {
-			decrypt(buf, recv_len, keya);/*recv_len-=16;*/
+			decrypt(buf, recv_len, keya);
 		}
 		buf[recv_len] = 0;
-		printf("recv_len: %d\n", recv_len);
+		printf("recv_len: %d\n", (int)recv_len);
 		fflush(stdout);
-		//printf("Data: %s\n" , buf);
-		int local_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		//local_me.sin_addr.s_addr=inet_addr("127.0.0.1");
-		setsockopt(local_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-		if (bind(local_fd, (struct sockaddr*) &local_me, slen) == -1) {
-			printf("socket bind error in chilld");
+
+		// prepare for reply
+		struct sockaddr_storage reply_me;
+		socklen_t reply_me_len;
+		int local_fd = socket(addr_family, SOCK_DGRAM, 0);
+		if (local_fd < 0) {
+			perror("socket");
 			exit(1);
 		}
-		int ret = connect(local_fd, (struct sockaddr *) &local_other, slen);//父进程替子进程做
-		if (fork() == 0)					//子
-				{
+		setsockopt(local_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+		// bind to the same local address/port as the listening socket
+		memcpy(&reply_me, &local_me, sizeof(local_me)); 
+		reply_me_len = slen_me;
+		if (bind(local_fd, (struct sockaddr*) &reply_me, reply_me_len) == -1) {
+			printf("socket bind error in child");
+			exit(1);
+		}
+		int ret = connect(local_fd, (struct sockaddr *) &local_other, slen_other);
+		if (fork() == 0) { // child
 			if (ret != 0) {
 				printf("connect return %d @1\n", ret);
 				exit(1);
 			}
 			close(local_listen_fd);
 
-			struct sockaddr_in remote_me, remote_other;
-
-			memset(&remote_other, 0, sizeof(remote_other));
-			remote_other.sin_family = AF_INET;
-			//printf("remote_address=%s  remote_port=%d\n",remote_address,remote_port);
-			remote_other.sin_port = htons(remote_port);
-			remote_other.sin_addr.s_addr = inet_addr(remote_address);
-			int remote_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-			ret = connect(remote_fd, (struct sockaddr *) &remote_other, slen);
+			struct sockaddr_storage remote_other;
+			socklen_t remote_other_len;
+			int remote_family = AF_UNSPEC;
+			if (resolve_addr(remote_address, remote_port, &remote_other, &remote_other_len, &remote_family) != 0) {
+				fprintf(stderr, "Failed to resolve remote address\n");
+				exit(1);
+			}
+			int remote_fd = socket(remote_family, SOCK_DGRAM, 0);
+			if (remote_fd < 0) {
+				perror("socket");
+				exit(1);
+			}
+			ret = connect(remote_fd, (struct sockaddr *) &remote_other, remote_other_len);
 			if (ret != 0) {
 				printf("connect return %d @2\n", ret);
 				exit(1);
 			}
 
-			if (keyb[0]) {/*recv_len+=16;*/
+			if (keyb[0]) {
 				encrypt(buf, recv_len, keyb);
 			}
 			ret = send(remote_fd, buf, recv_len, 0);
@@ -244,49 +285,44 @@ int main(int argc, char *argv[]) {
 				int n;
 				for (n = 0; n < nfds; ++n) {
 					if (events[n].data.fd == local_fd) {
-						recv_len = recv(local_fd, buf, buf_len, 0);
-						if (recv_len < 0) {
-							printf("recv return %d @1", recv_len);
+						ssize_t recv_len2 = recv(local_fd, buf, buf_len, 0);
+						if (recv_len2 < 0) {
+							printf("recv return %ld @1", recv_len2);
 							exit(1);
 						}
 						if (keya[0]) {
-							decrypt(buf, recv_len, keya);/*recv_len-=16;*/
+							decrypt(buf, recv_len2, keya);
 						}
-						buf[recv_len] = 0;
-						printf("len %d received from child@1\n", recv_len);
-						//printf("%s received from child@1\n",buf);
-						if (keyb[0]) {/*recv_len+=16;*/
-							encrypt(buf, recv_len, keyb);
+						buf[recv_len2] = 0;
+						printf("len %ld received from child@1\n", recv_len2);
+						if (keyb[0]) {
+							encrypt(buf, recv_len2, keyb);
 						}
-						//printf("before send %s\n",buf);
-						ret = send(remote_fd, buf, recv_len, 0);
+						ret = send(remote_fd, buf, recv_len2, 0);
 						if (ret < 0) {
 							printf("send return %d at @1", ret);
 							exit(1);
 						}
 						printf("send return %d @1\n", ret);
 					} else if (events[n].data.fd == remote_fd) {
-						recv_len = recv(remote_fd, buf, buf_len, 0);
-						if (recv_len < 0) {
-							printf("recv return -1 @2", recv_len);
+						ssize_t recv_len2 = recv(remote_fd, buf, buf_len, 0);
+						if (recv_len2 < 0) {
+							printf("recv return -1 @2");
 							exit(1);
 						}
 						if (keyb[0]) {
-							decrypt(buf, recv_len, keyb);/*recv_len-=16;*/
+							decrypt(buf, recv_len2, keyb);
 						}
-						buf[recv_len] = 0;
-						printf("len %d received from child@1\n", recv_len);
-						//printf("%s received from child@2\n",buf);
-						if (keya[0]) {/*recv_len+=16*/
-							;
-							encrypt(buf, recv_len, keya);
+						buf[recv_len2] = 0;
+						printf("len %ld received from child@2\n", recv_len2);
+						if (keya[0]) {
+							encrypt(buf, recv_len2, keya);
 						}
-						ret = send(local_fd, buf, recv_len, 0);
+						ret = send(local_fd, buf, recv_len2, 0);
 						if (ret < 0) {
 							printf("send return %d @2", ret);
 							exit(1);
 						}
-
 						printf("send return %d @2\n", ret);
 					}
 				}
@@ -295,8 +331,6 @@ int main(int argc, char *argv[]) {
 		} else {
 			close(local_fd);
 		}
-		//exit(0);
 	}
-
 	return 0;
 }
